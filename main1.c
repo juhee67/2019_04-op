aa5a51b84df6bb49e88faf637f789894c74d6861
juhee67 <55428174+juhee67@users.noreply.github.com>
2019-09-29 23:58:38 +0900

    initial

--- main1.c
+++ main1.c
@@ -4,17 +4,24 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-    
     int x, y;
+    int plus, minus, multyply, division, remainder;
     
-    printf("enter 2 integers :");
+    printf("enter 2 integers : 16 7");
     scanf("%d %d", &x, &y);
     
-    printf("plus is %i\n:", x+y);
-    printf("minus is %i\n", x-y);
+    plus = x+y;
+    minus = x-y;
+    multyply = x*y;
+    division = x/y;
+    remainder = x%y;
+
+    printf("plus is %i\n", x+y);
+	printf("minus is %i\n", x-y);
     printf("multyply is %i\n", x*y);
     printf("division is %i\n", x/y);
-    printf("%% is %i\n", x%y);
+    printf("remainder is %i\n", x%y);
+
 	
 	return 0;
 }
